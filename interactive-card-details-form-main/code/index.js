a12f1c9a580a3523c07e5facbe57652159718619
Unicatm <alexandra.tertiscu@gmail.com>
2023-10-23 21:26:35 +0300

    wip

--- interactive-card-details-form-main/code/index.js
+++ interactive-card-details-form-main/code/index.js
@@ -1,9 +1,21 @@
 let detailsOnCard = document.querySelectorAll("#onCard");
 let inputs = document.querySelectorAll("#inputCard");
 
+const contentFront = document.querySelectorAll(".content-front");
+
+const warning = document.createElement("p");
+
+function validation() {
+  if (detailsOnCard[1].textContent.length < 12) {
+    warning.textContent = "Uncompleted";
+    contentFront.appendChild(warning);
+  }
+}
+
 inputs.forEach((input, index) => {
   input.addEventListener("change", () => {
     detailsOnCard[index].textContent = input.value;
+    console.log(detailsOnCard[1].textContent.length);
+    validation();
   });
 });
-console.log(detailsOnCard);
